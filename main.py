a59edcb6204671db33fd7a56f6f76b1a4e9e47f3
tanuj_mehta <tanujm241@gmail.com>
2018-10-31 23:47:42 +0530

    Commit.0 - this is the first commit - made an empty shell of this project

---
+++ main.py
@@ -0,0 +1,7 @@
+import modules.first as first
+
+## this is the main program which connects all the modules.
+## Upon run of this project , this file is run first
+print('tanuj')
+
+first.a()
---
+++ modules/first.py
@@ -0,0 +1,5 @@
+## all the source code for the first module (part) of the project goes here
+## other modules will also be made as other python files under this folder 'modules'
+
+def a():
+    print('a')
