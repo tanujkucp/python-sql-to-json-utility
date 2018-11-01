606e9ee358e4f5c559850102cbf7806d70d1e323
tanuj_mehta <tanujm241@gmail.com>
2018-11-02 01:24:04 +0530

    Commit.2 - Completed json import to db

--- main.py
+++ main.py
@@ -3,56 +3,57 @@ import modules.json2java as j2java
 import modules.xml2db as x2db
 import modules.xml2json as x2j
 
+
 # this is the main program which connects all the modules.
 # Upon run of this project , this file is run first
 
 
 def showMainMenu():
     print('^^^^^^^^^^^^Welcome to the main menu^^^^^^^^^^^^\n')
-    print('What type of file do you have?\n1. JSON\t2. XML\n')
-    answer=int(input('Answer:'))
+    print('What type of file do you have?\n1. JSON \t2. XML\n')
+    answer = int(input('Answer:'))
     showNextMenu(answer)
 
 
 def showNextMenu(choice1):
-    if choice1==1:
+    if choice1 == 1:
         print('Choose an operation for JSON-\n1. JSON to SQL DB\t2. JSON to Java Class\n')
-        answer=int(input('Answer:'))
-        performJSON(choice1,answer)
-    elif choice1==2:
+        answer = int(input('Answer:'))
+        performJSON(choice1, answer)
+    elif choice1 == 2:
         print('Choose an operation for XML-\n1. XML to SQL DB\t2. XML to JSON\n')
         answer = int(input('Answer:'))
-        performXML(choice1,answer)
+        performXML(choice1, answer)
     else:
         print('Wrong selection!')
         showMainMenu()
 
 
-def performJSON(choice1,choice2):
+def performJSON(choice1, choice2):
     fileName = input('Enter JSON file name to convert(with .json):')
     if fileName.strip()[-4:] == 'json':
-        success=readFile(fileName)
-        if success!=None:
-            if choice2==1:
+        success = readFile(fileName)
+        if success is not None:
+            if choice2 == 1:
                 j2db.main(success)
-            elif choice2==2:
+            elif choice2 == 2:
                 j2java.main(success)
             else:
                 print('Wrong Selection')
                 showNextMenu(choice1)
         else:
             print('Error while opening specified file.Try different file name!\n')
-            performJSON(choice1,choice2)
+            performJSON(choice1, choice2)
     else:
         print('Filename should end with .json. Try Again!')
-        performJSON(choice1,choice2)
+        performJSON(choice1, choice2)
 
 
-def performXML(choice1,choice2):
+def performXML(choice1, choice2):
     fileName = input('Enter XML file name to convert(with .xml):')
     if fileName.strip()[-3:] == 'xml':
-        success=readFile(fileName)
-        if success!=None:
+        success = readFile(fileName)
+        if success is not None:
             if choice2 == 1:
                 x2db.main(success)
             elif choice2 == 2:
@@ -62,7 +63,7 @@ def performXML(choice1,choice2):
                 showNextMenu(choice1)
         else:
             print('Error while opening specified file.Try different file name!\n')
-            performXML(choice1,choice2)
+            performXML(choice1, choice2)
     else:
         print('Filename should end with .xml. Try Again!')
         performXML(choice1, choice2)
@@ -70,12 +71,13 @@ def performXML(choice1,choice2):
 
 def readFile(name):
     try:
-        file = open('data/'+name, 'r')
+        file = open('data/' + name, 'r')
         contents = file.read()
         return contents
     except IOError as e:
         print(e)
         return None
+
 
 # main function call that runs this code
 showMainMenu()
--- modules/json2db.py
+++ modules/json2db.py
@@ -4,4 +4,75 @@ import sqlite3 as sql
 
 
 def main(contents):
-    print(contents)
+    db = connectDB()
+    tuples = parseJSON(contents)
+    if dict is not None:
+        insertToDB(db, tuples)
+    fetch(db)
+
+
+def connectDB():
+    con = sql.connect('Primary.db')
+    # creating a new table
+    query = 'CREATE TABLE IF NOT EXISTS CONTACTS(id INT PRIMARY KEY, name VARCHAR(30), email VARCHAR(30), phone VARCHAR(15))'
+    con.execute(query)
+    con.commit()
+    return con
+
+
+def parseJSON(contents):
+    tuples = []
+    contents = contents.strip()
+    contents = contents[1:len(contents) - 1]
+    contents = contents.strip()
+    data = contents.split(':', 1)
+    if data[0].strip() != '"data"':
+        return None
+    else:
+        arr = data[1].strip()
+        arr = arr[1: len(arr) - 1]
+        arr = arr.strip()
+        arr = arr[1:len(arr)]
+        rows = arr.split('{')
+        for row in rows:
+            row = row.strip()
+            keys = row.split(',')
+            if len(keys) > 4:
+                del keys[len(keys)-1]
+            all=[]
+            for key in keys:
+                key = key.strip()
+                pair=key.split(':',2)
+                value=pair[1].strip()
+                value=value.split('"',2)
+                if value[0] != '':
+                    all.append(int(value[0]))
+                else:
+                    all.append(value[1])
+            tup=tuple(all)
+            tuples.append(tup)
+
+        return tuples
+
+
+def insertToDB(db, tuples):
+    try:
+        cursor = db.cursor()
+        cursor.executemany('''INSERT INTO CONTACTS(id, name, email, phone) VALUES(?,?,?,?)''', tuples)
+        db.commit()
+    except:
+        print('An error occurred while adding data!\n')
+
+
+def fetch(db):
+    try:
+        cursor=db.cursor()
+        cursor.execute('SELECT * FROM CONTACTS')
+        data=cursor.fetchall()
+        print('Table contains-\n')
+        for row in data:
+            print(row)
+    except:
+        print('An error occurred while fetching data!\n')
+
+
