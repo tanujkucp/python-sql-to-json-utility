300ba2451f694f4c124893e4981e2a47ce191d12
SHIVAM <shivamgurjar1008@gmail.com>
2018-11-05 01:26:15 +0530

    Done!

--- modules/xml2db.py
+++ modules/xml2db.py
@@ -1,4 +1,69 @@
-
+import sqlite3 as sql
 
 def main(contents):
-    print(contents)
+    dbCon = connectDB()
+    all = xmltoDB(contents)
+    if dict is not None:
+       insertToDB(dbCon,all)
+    fetch(dbCon)
+
+def connectDB():
+    con = sql.connect('Database.db')
+
+
+
+    query = 'CREATE TABLE IF NOT EXISTS CONTACTS(id INT PRIMARY KEY, name VARCHAR(30), email VARCHAR(30), phone VARCHAR(15))'
+    con.execute(query)
+    con.commit()
+    return con
+
+
+def xmltoDB(contents):
+    all=[]
+    contents = contents.strip()
+    content = contents.split('>',1)
+    array = content[1].strip()
+    new = array[:len(content[1])-len(content[0])-3]
+    new1 = new.strip()
+    sp = new.split('>',1)
+    tbname = sp[0]
+    spn = sp[0]+'>'
+    splitted  = new1.split(spn)
+    for row in splitted:
+        row = row.strip()
+        row1 = row[:-(len(spn)+1)]
+        row1 = row1.strip()
+        sprow = row1.split('<')
+        sprowf = sprow[1::2]
+        list = []
+        for i in sprowf:
+            i1 = i.split('>')
+            i2 = i1[1]
+            if i2.isdigit():
+                list.append(int(i2))
+            else:
+                i3 = '"'+i2+'"'
+                list.append(i3)
+        t = tuple(list)
+        all.append(t)
+        all1 = all[1:]
+    return all1
+
+def insertToDB(db, tuples):
+    try:
+        cursor = db.cursor()
+        cursor.executemany('''INSERT INTO CONTACTS(id, name, email, phone) VALUES(?,?,?,?)''', tuples)
+        db.commit()
+    except:
+        print('An error occurred while adding data!\n')
+
+def fetch(db):
+    try:
+        cursor=db.cursor()
+        cursor.execute('SELECT * FROM CONTACTS')
+        data=cursor.fetchall()
+        print('Table contains-\n')
+        for row in data:
+            print(row)
+    except:
+        print('An error occurred while fetching data!\n')
--- modules/xml2json.py
+++ modules/xml2json.py
@@ -1,4 +1,65 @@
 
+file = open('data1.xml', 'r')
+contents = file.read();
 
 def main(contents):
-    print(contents)
+    str = xml2json(contents)
+    writeFile(str)
+
+def xml2json(contents):
+    all = []
+    str2 =''
+    str3 = ''
+    contents = contents.strip()
+    content = contents.split('>', 1)
+    array = content[1].strip()
+    mainName = content[0]
+    mainName = mainName[1:]
+    new = array[:len(content[1]) - len(content[0]) - 3]
+    new1 = new.strip()
+    sp = new.split('>', 1)
+    tbname = sp[0]
+    spn = sp[0] + '>'
+    splitted = new1.split(spn)
+    for row in splitted:
+        row = row.strip()
+        row1 = row[:-(len(spn) + 1)]
+        row1 = row1.strip()
+        sprow = row1.split('<')
+        sprowf = sprow[1::2]
+        list = []
+
+
+        for i in sprowf:
+            i1 = i.split('>')
+            i2 = i1[1]
+            j1 = i1[0]
+            j1=j1.strip()
+            if i2.isdigit():
+                i3 = i2
+            else:
+                i3 = '"' + i2 + '"'
+            i3 = '"'+j1+'"'+' : '+i3+',\n'
+            list.append(i3)
+        str = ''.join(list)
+        str = str[:-2]
+        str1 = '\n{\n' + str + '},'
+        all.append(str1)
+    all1 = all[1:]
+    str2 = ''.join(all1)
+    str2 = str2[:-1]
+    str3 = '{\n'+'"'+mainName+'"'+':['+str2+'\n]\n}'
+
+    return str3
+
+def writeFile(str):
+    try:
+        filename = input('Enter filename you want to save as json:')
+        f = open(filename + '.json', 'w+')
+        f.write(str)
+    except:
+        print("File name already exists, try another name")
+
+
+
+main(contents)
