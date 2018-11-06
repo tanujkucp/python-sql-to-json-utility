7f12d00212b5b10a865163f1b5a11e3a555f8c46
tanuj_mehta <tanujm241@gmail.com>
2018-11-06 15:45:35 +0530

    Commit.7 - merged with shivam after ordering problem solved from xml2db

--- modules/xml2db.py
+++ modules/xml2db.py
@@ -1,23 +1,16 @@
 import sqlite3 as sql
 
 
-#file = open('data1.xml', 'r')
-#contents = file.read();
-
-
-
 def main(contents):
     dbCon = connectDB()
     all = xmltoDB(contents)
-    if dict is not None:
-       insertToDB(dbCon,all)
+    if dbCon is not None and all is not None:
+        insertToDB(dbCon, all)
     fetch(dbCon)
+
 
 def connectDB():
     con = sql.connect('Data.db')
-
-
-
     query = 'CREATE TABLE IF NOT EXISTS CONTACTS(id INT PRIMARY KEY, name VARCHAR(30), email VARCHAR(30), phone VARCHAR(15))'
     con.execute(query)
     con.commit()
@@ -25,19 +18,19 @@ def connectDB():
 
 
 def xmltoDB(contents):
-    all=[]
+    all = []
     contents = contents.strip()
-    content = contents.split('>',1)
+    content = contents.split('>', 1)
     array = content[1].strip()
-    new = array[:len(content[1])-len(content[0])-3]
+    new = array[:len(content[1]) - len(content[0]) - 3]
     new1 = new.strip()
     sp = new.split('>', 1)
-    spn = sp[0]+'>'
+    spn = sp[0] + '>'
     spn1 = new1.split(spn)
 
     for row in spn1:
         row = row.strip()
-        row1 = row[:-(len(spn)+1)]
+        row1 = row[:-(len(spn) + 1)]
         row1 = row1.strip()
         sprow = row1.split('<')
         sprowf = sprow[1::2]
@@ -52,29 +45,31 @@ def xmltoDB(contents):
             if value.isdigit():
                 dict[key] = int(value)
             else:
-                dict[key] =value
+                dict[key] = value
         if dict:
-           t = (dict['id'],dict['name'],dict['email'],dict['mobile'])
-           t = tuple(t)
-           all.append(t)
+            t = (dict['id'], dict['name'], dict['email'], dict['mobile'])
+            t = tuple(t)
+            all.append(t)
     return all
+
 
 def insertToDB(db, tuples):
     try:
         cursor = db.cursor()
         cursor.executemany('''INSERT INTO CONTACTS(id, name, email, phone) VALUES(?,?,?,?)''', tuples)
         db.commit()
+        print('Data imported successfully!')
     except:
         print('An error occurred while adding data!\n')
 
+
 def fetch(db):
     try:
-        cursor=db.cursor()
+        cursor = db.cursor()
         cursor.execute('SELECT * FROM CONTACTS')
-        data=cursor.fetchall()
+        data = cursor.fetchall()
         print('Table contains-\n')
         for row in data:
             print(row)
     except:
         print('An error occurred while fetching data!\n')
-#main(contents)
